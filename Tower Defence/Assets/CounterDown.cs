7b8a6b63850b5d07ffd0628a61a0989dde67d51c
JamesMacroZhang <zhang1997226@qq.com>
2019-11-09 12:46:10 +0100

    Add new components & Modify UI.
    In the game interface, increased the countdown and counter.In the menu interface, the background image of the menu has been modified.

---
+++ Tower Defence/Assets/CounterDown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CounterDown : MonoBehaviour
+{
+    public GameObject ti;
+    public GameObject ni;
+    public int end = 1;
+    private int TimeControl = 6; //若为public，Update函数不能调用
+    //public bool ClickNum = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ni = GameObject.Find("nice");
+        ti = GameObject.Find("time");
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        //if (ClickNum == true)
+        //{
+            if (end > 0)
+            {
+                end = TimeControl - (int)Time.time;
+                ni.GetComponent<Text>().text = "" + end;
+            }
+
+            if (end == 0)
+            {
+                ti.GetComponent<Text>().text = "Time up";
+            }
+        //}
+    }
+    //public void Click() 
+    //{
+        //ClickNum = true;
+        
+    //}
+}
