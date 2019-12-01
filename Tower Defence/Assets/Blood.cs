2cce2985b8477c24514cb3141510c3362005f629
chensimon95 <chensimon95@gmail.com>
2019-12-01 15:12:35 +0100

    update
    Blood

---
+++ Tower Defence/Assets/Blood.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blood : MonoBehaviour
+{
+    public float HP;
+    public Slider HPSlider;
+    private float hp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void updateBlood(int damage) {
+        hp = hp + damage;
+        HPSlider.value = hp/HP;
+    }
+}
