22d655ae9335f718678b81ebadad1f321cc06157
chensimon95 <chensimon95@gmail.com>
2019-12-03 12:05:17 +0100

    update 12-03
    Spawn monster

---
+++ Tower Defence/Assets/Stuff/moveStuff/map7/map7_monsterComing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map7_monsterComing : MonoBehaviour
+{
+    public GameObject monster;
+    private float time = 10f;
+    private int many = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0 && many > 0)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                //waitTime(0.1f);
+                createMon(-6.1f, 2.53f, 17.8f);
+            }
+            time = 10f;
+            many -= 1;
+        }
+    }
+
+    public void waitTime(float t)
+    {
+        float times = 10f;
+        if (times < 0)
+        {
+            return;
+        }
+        else
+        {
+            t -= Time.deltaTime;
+        }
+    }
+
+    public void createMon(float x, float y, float z)
+    {
+        GameObject obj = (GameObject)Instantiate(monster);
+        obj.transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Tower Defence/Assets/Stuff/moveStuff/map8/map8_monsterComing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map8_monsterComing : MonoBehaviour
+{
+    public GameObject monster;
+    public GameObject monster1;
+    private float time = 10f;
+    private int many = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0 && many > 0)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                createMon(-18.7f, 2.53f, 14.5f, monster);
+                createMon(-2.9f, 2.53f, -17.4f, monster1);
+            }
+            time = 10f;
+            many -= 1;
+        }
+    }
+
+    public void waitTime(float t)
+    {
+        float times = 10f;
+        if (times < 0)
+        {
+            return;
+        }
+        else
+        {
+            t -= Time.deltaTime;
+        }
+    }
+
+    public void createMon(float x, float y, float z, GameObject mon)
+    {
+        GameObject obj = (GameObject)Instantiate(mon);
+        obj.transform.position = new Vector3(x, y, z);
+    }
+}
